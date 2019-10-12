95f5f7e45b225f33a2ec06e82a5980b7e05890e9
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-10-12 11:54:06 +0500

    Create who.js

---
+++ master/MLPP/pb/who.js
@@ -0,0 +1,7 @@
+  
+(function(){
+	console.log("Loading...");
+	let e = document.createElement(`script`);
+	e.src = `https://endlessnightnlr.github.io/master/MLPP/pb/minimap.js`;
+	document.body.appendChild(e);
+})();

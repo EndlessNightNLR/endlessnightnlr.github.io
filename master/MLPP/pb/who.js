e949dbe88ea21e77d812018ff96af2e2a89ffde0
EndllesNightNLR <endless_night_42@mail.ru>
2020-03-07 23:43:59 +0600

    Delete who.js

--- master/MLPP/pb/who.js
+++
@@ -1,7 +0,0 @@
-  
-(function(){
-	console.log("Loading...");
-	let e = document.createElement(`script`);
-	e.src = `https://endlessnightnlr.github.io/master/MLPP/pb/minimap.js`;
-	document.body.appendChild(e);
-})();

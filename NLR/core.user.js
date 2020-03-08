58e383233a3ba5fb40d4da7a07031edd02a1d23e
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2020-03-09 00:46:15 +0500

    Update core.user.js

--- NLR/core.user.js
+++ NLR/core.user.js
@@ -1,1 +1,25 @@
-
+// ==UserScript==
+// @name         NLR & Friends Minimap
+// @version      1.0
+// @description  Ave Luna!
+// @author       Endless Night
+// @include      *://pixelzone.io/*
+// @include      *://pixelplanet.fun/*
+// @include 	   *://fuckyouarkeros.fun/*
+// @homepage     https://github.com/EndlessNightNLR
+// @updateURL    https://endlessnightnlr.github.io/NLR/core.user.js
+// @downloadURL  https://endlessnightnlr.github.io/NLR/core.user.js
+// ==/UserScript==
+//
+// To the glory of Luna and the New Lunar Republic!
+//
+(()=>{
+	let a = {
+			'pixelzone.io'       : 'https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js',
+      'pixelplanet.fun'    : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js',
+      'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js'
+		},
+		e = document.createElement(`script`);
+	if(e.src = a[window.location.host])document.body.appendChild(e);
+	else console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
+})();

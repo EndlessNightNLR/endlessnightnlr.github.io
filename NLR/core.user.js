ae756c9b9e3e0d7458c532b2ae04e33397ea0b34
EndllesNightNLR <endless_night_42@mail.ru>
2020-03-26 18:29:50 +0600

    Update core.user.js

--- NLR/core.user.js
+++ NLR/core.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         NLR & Friends Minimap
-// @version      1.0
+// @version      1.1
 // @description  Ave Luna!
 // @author       Endless Night
 // @include      *://pixelzone.io/*
 // @include      *://pixelplanet.fun/*
-// @include 	   *://fuckyouarkeros.fun/*
+// @include 	 *://fuckyouarkeros.fun/*
 // @homepage     https://github.com/EndlessNightNLR
 // @updateURL    https://endlessnightnlr.github.io/NLR/core.user.js
 // @downloadURL  https://endlessnightnlr.github.io/NLR/core.user.js
@@ -13,13 +13,11 @@
 //
 // To the glory of Luna and the New Lunar Republic!
 //
-(()=>{
-	let a = {
-			'pixelzone.io'       : 'https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js',
-      'pixelplanet.fun'    : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js',
-      'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js'
-		},
-		e = document.createElement(`script`);
-	if(e.src = a[window.location.host])document.body.appendChild(e);
-	else console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
-})();
+{
+	let e = document.createElement(`script`);
+	(e.src = {
+		'pixelzone.io'       : 'https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js',
+    	'pixelplanet.fun'    : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js',
+    	'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/NLR/PixelPlanet/code.user.js'
+	}[window.location.host]) ? document.body.appendChild(e) : console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
+};

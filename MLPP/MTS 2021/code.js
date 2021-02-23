c6e6b4aabddd34245626baf874953e63967978ec
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2021-02-23 20:57:00 +0500

    Update code.js

--- MLPP/MTS 2021/code.js
+++ MLPP/MTS 2021/code.js
@@ -1,26 +1,4 @@
-// ==UserScript==
-// @name         NLR Minimap Test
-// @version      2.7.2.3
-// @description  Ave Luna!
-// @author       Endless Night
-// @include      https://pixelwar-mts.ru/*
-// @homepage     https://github.com/EndlessNightNLR
-// @updateURL    https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/PixelPlanet/code.js
-// @downloadURL  https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/PixelPlanet/code.js
-// ==/UserScript==
-//
-// To the glory of Luna and the New Lunar Republic
-//
-//УДАЛЕНИЕ СКРИПТА ПОСЛЕ ИНИЦИАЛИЗАЦИИ
-//BEGIN
-/*
-Array.from(document.body.children).forEach(e => {
-	if(e.nodeName === "SCRIPT" && !e.src)
-		document.body.removeChild(e);
-});
-*/
-//END
-
+(() => {
 //START MAIN MAP CODE
 function initNLRM() {
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
@@ -3101,3 +3079,4 @@ function isFrame(){
     };
     return isFramed;
 }
+});

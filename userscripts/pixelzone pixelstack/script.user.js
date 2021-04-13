178f46f558c9785f477cb529de7763f2a71b21b1
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2021-04-13 15:01:19 +0500

    Update script.user.js

--- userscripts/pixelzone pixelstack/script.user.js
+++ userscripts/pixelzone pixelstack/script.user.js
@@ -1,1 +1,12 @@
+// ==UserScript==
+// @name         Pixelzone pixelstack
+// @version      1.0
+// @description  fuck
+// @author       Endless Night
+// @match        https://pixelzone.io/*
+// @icon         https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/images/NLRicon.png
+// ==/UserScript==
 
+const script = document.createElement('script');
+script.src = 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/userscripts/pixelzone%20pixelstack/code.js';
+document.body.appendChild(script);
